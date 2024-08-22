3022dd13723bed8995e40ac687e6f10ce9f8f5cc
aworles <khanhanna.vu2020@gmail.com>
2024-08-22 19:38:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["discord.gg/brawlparadise","JOIN NOW!","Tabal loves Ino <:Peepo_UwU:1247299495276970045>"];
 
 
 let currentIndex = 0;
